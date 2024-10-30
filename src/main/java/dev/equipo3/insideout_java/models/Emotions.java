c049591a7c9babce66a82bf08013712e2afc877d
Alberto Casas <alberto.casas.ofiuco@gmail.com>
2024-10-30 15:04:17 +0100

    feat: [feature/typeemotion] enum list

--- src/main/java/dev/equipo3/insideout_java/models/Emotions.java
+++ src/main/java/dev/equipo3/insideout_java/models/Emotions.java
@@ -1,5 +1,11 @@
 package dev.equipo3.insideout_java.models;
 
-public class Emotions {
-    
+public enum Emotions {
+    Joy, Sadness, Anger, Disgust, Fear, Anxiety, Envy, Shame, Boredom, Nostalgia;
+
+    public static Emotions getByIndex(int index) {
+        if (index < 1||index > values().length)
+        return null;
+        return values()[index -1];
+    }
 }
